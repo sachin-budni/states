82fa33ba2b2ee5ae58d8cbcc1d9687918d726d0a
unknown <sachinswapna143@gmail.com>
2019-11-13 09:50:28 +0530

    adding access control code

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
-
+const bodyParser = require('body-parser');
 let app = express();
+
+app.use(bodyParser.json({limit: '50mb'}))
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method == "OPTIONS"){
+        res.header("Access-Control-Allow-Methods",'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next();
+})
 
 app.get("/",(req,res)=>{
     res.send(states)
